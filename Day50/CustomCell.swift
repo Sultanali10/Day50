c37d03b18bae0ec7704979c7850448e7604ae8bd
Sultan Ali <sultanjamali1986@gmail.com>
2020-06-30 20:31:54 +0300

    adding DetailVC

---
+++ Day50/CustomCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCell.swift
+//  Day50
+//
+//  Created by Sultan Ali on 28/06/2020.
+//  Copyright © 2020 Sultan Ali. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class CustomCell: UITableViewCell{
+
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var cellLabel: UILabel!
+    
+}
---
+++ Day50/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  Day50
+//
+//  Created by Sultan Ali on 28/06/2020.
+//  Copyright © 2020 Sultan Ali. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var detailLabe: UILabel!
+    
+    @IBOutlet var detailImage: UIImageView!
+}
